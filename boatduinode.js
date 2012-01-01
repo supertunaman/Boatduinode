7482fe7b18a64be3f16d05f470dd72d35bfe604d
Tuna <tuna@supertunaman.com>
2012-01-01 14:59:26 -0600

    underping...

--- boatduinode.js
+++ boatduinode.js
@@ -1,5 +1,24 @@
+/* Copyright (C) 2011 by ath (@supertunaman)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE. */
+
 var http = require('http');
-<<<<<<< HEAD
 var io = require('socket.io');
 var express = require('express');
 
@@ -31,9 +50,3 @@ socket.on('connection', function(client) {
 
     })
 });
-=======
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (boatduinode) is running..');
-}).listen(8617);
->>>>>>> 35234ccb07ae7934a5b974b1db955cd8a1401075
